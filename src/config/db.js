2f5807ca5980412c736e5edf9a0e61403c3acc1d
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-03-21 20:44:11 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://theory:theory_123@cluster0.fya2r.mongodb.net/theory-db"
+    "mongodb+srv://theory_123:Gold123@cluster0.wapdf.mongodb.net/theory-db"
   );
 };
 
